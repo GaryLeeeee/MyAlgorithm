1c835668fbe52135e84001c2676527c280a45939
GaryLeeeee <664306561@qq.com>
2019-07-20 23:25:09 +0800

    快排(Java实现)

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,47 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by GaryLee on 2019-07-19 20:17.
+ */
+public class QuickSort{
+
+    public static int[] sortQuick(int[] arr, int left, int right){
+        int pivot = arr[left];//因为已经获取到值了，所以后面随便swap
+        int start = left;//左哨兵(坐标)
+        int end = right;
+        while(start<end){
+            //从右往左遍历，将小于pivot的移动左边去
+            //否则右哨兵--
+            while (start<end && arr[end] >=pivot){
+                end--;//
+            }
+            arr[start] = arr[end];
+            while(start<end&&arr[start]<pivot){
+                start++;
+            }
+            arr[end] = arr[start];
+        }
+        //运行到这里说明start=end,就把这个数设为pivot
+        //用start与end坐标均可
+        arr[start] = pivot;
+        if(start > left)
+            sortQuick(arr,left,start-1);
+        if(end < right)
+            sortQuick(arr,start+1,right);
+
+
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr  = {3,5,8,1,2,9,4,7,6};
+        for(Integer i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+        int[] after = sortQuick(arr,0,arr.length-1);
+        for(Integer i:after)
+            System.out.print(i+" ");
+    }
+}
